7da49aa344f294af577b30d902982ccb98a49f95
Victor Bo <10667379@qq.com>
2024-05-05 15:29:42 +0800

    feat: 调整系统信息获取方式，确保获取最准确的系统信息

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
 import VConsole from 'vconsole'
 import { isProd } from '@/utils/is'
 // #endif
+import { useSystemStore } from '@/stores/modules/system'
+
+const systemStore = useSystemStore()
 
 onLaunch(async () => {
   console.log('App Launch')
@@ -15,6 +18,13 @@ onShow(() => {
   if (!isProd())
     new VConsole()
   // #endif
+
+  // 确保获取的系统信息是最新且最正确的
+  nextTick(() => {
+    const systemInfo = uni.getSystemInfoSync()
+    console.log('系统信息 => ', systemInfo)
+    systemStore.setSystemInfo(systemInfo)
+  })
 })
 onHide(() => {
   console.log('App Hide')
--- src/stores/modules/system.ts
+++ src/stores/modules/system.ts
@@ -1,54 +1,85 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+
+interface SystemState {
+  screenHeight: number
+  statusBarHeight: number
+  navBarHeight: number
+  navHeight: number
+  tabHeight: number
+  safeBottomHeight: number
+  tabBarHeight: number
+  tabPageHeight: number
+}
 
 export const useSystemStore = defineStore(
   'system',
-  () => {
-    // 系统信息
-    const systemInfo = uni.getSystemInfoSync()
-    console.log('系统信息 => ', systemInfo)
+  {
+    state: (): SystemState => ({
+      screenHeight: 0,
+      statusBarHeight: 0,
+      navBarHeight: 0,
+      navHeight: 0,
+      tabHeight: 0,
+      safeBottomHeight: 0,
+      tabBarHeight: 0,
+      tabPageHeight: 0,
+    }),
+    actions: {
+      setSystemInfo(systemInfo: UniApp.GetSystemInfoResult) {
+        console.log('系统信息 => ', systemInfo)
 
-    // 屏幕高度
-    const screenHeight = ref<number>(systemInfo?.screenHeight ?? 0)
-    console.log('屏幕高度 => ', screenHeight.value)
+        // 屏幕高度
+        const screenHeight = systemInfo?.screenHeight ?? 0
+        console.log('屏幕高度 => ', screenHeight)
 
-    // 状态栏高度
-    const statusBarHeight = ref<number>(systemInfo?.statusBarHeight ?? 0)
-    console.log('状态栏高度 => ', statusBarHeight.value)
+        // 状态栏高度
+        const statusBarHeight = systemInfo?.statusBarHeight ?? 0
+        console.log('状态栏高度 => ', statusBarHeight)
 
-    // 导航栏高度
-    const navBarHeight = ref<number>(40)
-    console.log('导航栏高度 => ', navBarHeight.value)
+        // 导航栏高度
+        let navBarHeight = 40
+        console.log('导航栏高度 => ', navBarHeight)
 
-    // 顶部总高度
-    const navHeight = ref<number>(statusBarHeight.value + navBarHeight.value)
-    console.log('顶部总高度 => ', navHeight.value)
+        // #ifdef MP-WEIXIN
+        // 微信小程序胶囊位置
+        const capsule = wx.getMenuButtonBoundingClientRect()
+        navBarHeight = capsule.height + (capsule.top - statusBarHeight) * 2
+        // #endif
 
-    // 底部安全区高度
-    const safeBottomHeight = ref<number>(screenHeight.value - (systemInfo?.safeArea?.bottom ?? screenHeight.value))
-    console.log('底部安全区高度 => ', safeBottomHeight.value)
+        // 顶部总高度
+        const navHeight = statusBarHeight + navBarHeight
+        console.log('顶部总高度 => ', navHeight)
 
-    // 底部导航栏高度
-    const tabBarHeight = ref<number>(50)
-    console.log('底部导航栏高度 => ', tabBarHeight.value)
+        // 底部安全区高度
+        const safeBottomHeight = screenHeight - (systemInfo?.safeArea?.bottom ?? screenHeight)
+        console.log('底部安全区高度 => ', safeBottomHeight)
 
-    // 底部总高度
-    const tabHeight = ref<number>(tabBarHeight.value + safeBottomHeight.value)
-    console.log('底部总高度 => ', tabHeight.value)
+        // 底部导航栏高度
+        let tabBarHeight = screenHeight - (systemInfo?.windowHeight ?? screenHeight) - safeBottomHeight
+        // #ifdef H5
+        tabBarHeight = 50
+        // #endif
+        console.log('底部导航栏高度 => ', tabBarHeight)
 
-    // 导航页面高度
-    const tabPageHeight = ref<number>(screenHeight.value - navHeight.value - tabHeight.value)
-    console.log('导航页面高度 => ', tabPageHeight.value)
+        // 底部总高度
+        const tabHeight = tabBarHeight + safeBottomHeight
+        console.log('底部总高度 => ', tabHeight)
 
-    return {
-      screenHeight,
-      statusBarHeight,
-      navBarHeight,
-      navHeight,
-      tabHeight,
-      safeBottomHeight,
-      tabBarHeight,
-      tabPageHeight,
-    }
+        // 导航页面高度
+        const tabPageHeight = screenHeight - navHeight - tabHeight
+        console.log('导航页面高度 => ', tabPageHeight)
+
+        this.$patch({
+          screenHeight,
+          statusBarHeight,
+          navBarHeight,
+          navHeight,
+          tabHeight,
+          safeBottomHeight,
+          tabBarHeight,
+          tabPageHeight,
+        })
+      },
+    },
   },
 )
